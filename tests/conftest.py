6f24a6052355fb9a5a991193533edef90aeb9323
Aman Monga <aman260@outlook.com>
2022-06-06 08:50:53 -0700

    ci: use correct redis url

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,21 @@
+import pytest
+
+
 pytest_plugins = [
     "tests.fixtures.app",
     "tests.fixtures.websocket",
     "tests.fixtures.redis"
 ]
+
+
+@pytest.fixture
+def redis_connection_string(request):
+    return request.config.getoption("--redis-connection-string")
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--redis-connection-string",
+        action="store",
+        default="redis://localhost:6379",
+    )
--- tests/fixtures/app.py
+++ tests/fixtures/app.py
@@ -4,11 +4,6 @@ from virtool_ws.app import create_app
 
 
 @pytest.fixture
-def redis_connection_string():
-    return "redis://localhost:6379"
-
-
-@pytest.fixture
 async def app(aiohttp_client, redis_connection_string):
     app = create_app(redis_connection_string)
     _ = await aiohttp_client(app)
